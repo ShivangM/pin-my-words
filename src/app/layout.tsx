ec3799f58f134d5a281e6852369f06339d2356b1
ShivangM <shivangmishra0824@gmail.com>
2023-09-22 22:49:36 +0530

    Metabase URL updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   },
   description:
     'Pin My Words is a platform for aspirants of competitive exams like CAT and GRE to store and learn new words every day. Users can log in and create a board, which can be private or collaborative, depending on whether they want to invite friends or not. The words can be sorted by root words and the platform will keep track of the learning progress. Users can also opt to generate examples and meanings for a specific word using AI. Moreover, users can take tests and quizzes based on the words they have learned so far and compete with others on a leaderboard if the board is collaborative.',
-  metadataBase: new URL(process.env.VERCEL_URL || 'http://localhost:3000/'),
+  metadataBase: new URL('https://pin-my-words.vercel.app/'),
   openGraph: {
     title: {
       template: '%s | Pin My Words',
